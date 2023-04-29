68dafd1c998e0b04d67898538a9e21f99aa7bc3a
abbi4code <113784630+abbi4code@users.noreply.github.com>
2023-04-30 04:08:04 +0530

    added 3 new

---
+++ main.js
@@ -0,0 +1,64 @@
+const SEARCH_URL ='https://api.themoviedb.org/3/search/movie?api_key=bb6db343ef4ab9210a52a3bfbfe258eb&query="';
+
+ const API_URL = " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bb6db343ef4ab9210a52a3bfbfe258eb"
+
+ const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/"
+
+ 
+
+ const form=document.getElementById('form')
+ const search=document.getElementById('search')
+ const main=document.getElementById('main')
+
+ getMovies(API_URL)
+ async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+    displayMovies(data.results)
+    console.log(data.results);
+ }
+ function displayMovies(movies){
+    main.innerHTML=''
+    movies.forEach((movie)=>{
+        const {title,poster_path,vote_average,overview}=movie
+        const moviesElement=document.createElement('div')
+        moviesElement.classList.add('movie')
+        moviesElement.innerHTML=`
+        <img src="${IMAGE_PATH + poster_path}" alt="${title}" />
+        <div class='movie-info'>
+        <h3>${title}</h3>
+        <span class="${getClassesByRating(vote_average)}"> ${vote_average}</span>
+        <div class='overview'>
+        <h3>Overview</h3>
+        ${overview}
+        </div>
+        </div>
+        `
+
+        main.appendChild(moviesElement)
+    })
+ }
+ function getClassesByRating(rating){
+    if(rating>=8){
+        return 'green'
+    }else if(rating>=5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+ }
+ form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    searchValue=search.value
+    if(searchValue && searchValue !==''){
+        getMovies(SEARCH_URL+searchValue)
+        searchValue=''
+    }else{
+        window.location.reload()
+    }
+
+})
+
+search.addEventListener('click',()=>{
+    style.backgroundColor='white'
+})
